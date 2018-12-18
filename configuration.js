6b341c1bbc9adb925a8076cba8a6e812d6c8de1c
PrabodhGhosh <ghosh.prabodh@gmail.com>
2018-12-18 09:07:03 +0530

    New commit

--- configuration.js
+++ configuration.js
@@ -1,34 +1,40 @@
 var basePath = __dirname;
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
+
 exports.config = {
 		seleniumAddress: 'http://localhost:4444/wd/hub',
 		specs: ['Regression/SubmitFormWithDataSet.js'],
 		capabilities: {
 			'browserName': 'chrome'
 		},
+		
+		jasmineNodeOpts: {
+		    showColors: true, // Use colors in the command line report.
+		  },
+
 
 		onPrepare : function()
 		{
-			browser.driver.manage().window().maximize();
+			browser.driver.manage().window().maximize(); //Maximize the window
 
 			jasmine.getEnv().addReporter(
 					new Jasmine2HtmlReporter({
-						savePath: 'target/screenshots'
+						savePath: 'target/screenshots' //Generate HTML Report
 					})
 			);
 
 
 			global.requirePageObjects = function (relativePath) {
-				return require(basePath + '/PageObjects/' + relativePath + '.js');
+				return require(basePath + '/PageObjects/' + relativePath + '.js'); // Access multi level page objects
 			};
 
 			global.requireHelper = function (relativePath) {
-				return require(basePath + '/Helper/' + relativePath + '.js');
+				return require(basePath + '/Helper/' + relativePath + '.js'); // Access multi level helper utilities
 			};
 
 			global.requireData = function (relativePath) {
-				return require(basePath + '/DataSource/' + relativePath + '.js');
+				return require(basePath + '/DataSource/' + relativePath + '.js'); // Access multi level data source
 			};
 		}
 };
